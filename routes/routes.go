e956c079b065fd3277613a818659dec081b8c9d1
Vini <vsgh-gouveia@hotmail.com>
2023-09-12 18:46:19 -0300

    CORS, rotas logout e validate
    CORS atualizado e rotas logout e validate mudadas de GET para POST

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	"github.com/medellinoriginalcompany/api/controllers"
 	"github.com/medellinoriginalcompany/api/handlers"
 	"github.com/medellinoriginalcompany/api/middleware"
@@ -11,11 +12,17 @@ func HandleRequest() {
 	//* Gerenciamento de rotas
 	r := gin.Default()
 
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowCredentials: true,
+		AllowHeaders: []string{"Origin, X-Requested-With, Content-Type, Accept"},
+	}))
+
 	r.GET("/example", handlers.Example)
 	r.POST("/registro", controllers.Signup)
 	r.POST("/login", controllers.Login)
-	r.GET("/logout", controllers.Logout)
-	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
+	r.POST("/logout", middleware.RequireAuth, controllers.Logout)
+	r.POST("/validate", middleware.RequireAuth, controllers.Validate)
 
 	r.Run()
 }
