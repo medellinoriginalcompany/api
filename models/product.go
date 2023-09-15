2094052ab6f66a5f84ab7744ef608c9d69d36c33
Vini <vsgh-gouveia@hotmail.com>
2023-09-15 20:29:32 -0300

    Primeiro modelo da entidade produto

--- models/product.go
+++ models/product.go
@@ -1,7 +1,34 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Product struct {
-	gorm.Model
+	ID int32 `gorm:"primaryKey;"`
+
+	Name        string  `gorm:"type:varchar(255);notnull"`
+	Description string  `gorm:"type:varchar(255);notnull"`
+	SKU         string  `gorm:"type:varchar(50);unique;notnull"`
+	Price       float32 `gorm:"notnull"`
+	Stock       int32   `gorm:"notnull"`
+	Active      bool    `gorm:"notnull;default:true"`
+	Discount    float32 `gorm:"notnull"`
+	Banner      string  `gorm:"type:varchar(255);notnull"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
+	CategoryID int32    `gorm:"notnull"`
+	Category   ProductCategory `gorm:"foreignKey:CategoryID"`
+
+	SizeID int32 `gorm:"notnull"`
+	Size   ProductSize  `gorm:"foreignKey:SizeID"`
+}
+
+func (Product) TableName() string {
+	return "products.products"
 }
