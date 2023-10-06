2ca813e2678cc8507c5f92df597b5624920c5cb8
Vini <vsgh-gouveia@hotmail.com>
2023-10-06 20:10:01 -0300

    Nomes de todas rotas para PTBR e rota de edição produto

--- routes/routes.go
+++ routes/routes.go
@@ -26,24 +26,25 @@ func HandleRequest() {
 	r.POST("/login", controllers.Login)
 	r.POST("/logout", middleware.RequireAuth, controllers.Logout)
 
-	r.POST("/validate", middleware.RequireAuth, controllers.Validate)
-	r.POST("/admin/validate", middleware.RequireAdmin, controllers.Validate)
+	r.POST("/validar", middleware.RequireAuth, controllers.Validate)
+	r.POST("/admin/validar", middleware.RequireAdmin, controllers.Validate)
 
 	r.POST("/admin/registro", middleware.RequireAdmin, controllers.AdminSignup)
 	r.POST("/admin/login", controllers.AdminLogin)
 	r.POST("/admin/logout", middleware.RequireAdmin, controllers.AdminLogout)
 	r.POST("/admin/cadastrar-produto", middleware.RequireAdmin, controllers.AddProduct)
 	
-	r.GET("/admin/products", middleware.RequireAdmin, controllers.GetProducts)
-	r.GET("/admin/products/properties", middleware.RequireAdmin, controllers.GetProductProperties)
+	r.GET("/admin/produtos", middleware.RequireAdmin, controllers.GetProducts)
+	r.GET("/admin/produtos/propriedades", middleware.RequireAdmin, controllers.GetProductProperties)
+	r.GET("/admin/produto/:id", middleware.RequireAdmin, controllers.GetProduct)
 
-	r.POST("/admin/products/add-property/tamanhos", middleware.RequireAdmin, controllers.AddProductSize)
-	r.POST("/admin/products/add-property/cores", middleware.RequireAdmin, controllers.AddProductColor)
-	r.POST("/admin/products/add-property/categorias", middleware.RequireAdmin, controllers.AddProductCategory)
-	r.POST("/admin/products/add-property/tipos", middleware.RequireAdmin, controllers.AddProductType)
+	r.POST("/admin/produtos/adicionar-propriedade/tamanhos", middleware.RequireAdmin, controllers.AddProductSize)
+	r.POST("/admin/produtos/adicionar-propriedade/cores", middleware.RequireAdmin, controllers.AddProductColor)
+	r.POST("/admin/produtos/adicionar-propriedade/categorias", middleware.RequireAdmin, controllers.AddProductCategory)
+	r.POST("/admin/produtos/adicionar-propriedade/tipos", middleware.RequireAdmin, controllers.AddProductType)
 	
-	r.DELETE("/admin/delete-product/:id", middleware.RequireAdmin, controllers.DeleteProduct)
-	r.DELETE(("/admin/delete/:type/:id"), middleware.RequireAdmin, controllers.DeleteProductProperty)
+	r.DELETE("/admin/deletar-produto/:id", middleware.RequireAdmin, controllers.DeleteProduct)
+	r.DELETE(("/admin/deletar/:type/:id"), middleware.RequireAdmin, controllers.DeleteProductProperty)
 
 	r.GET("/example", handlers.Example)
 
