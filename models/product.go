ecb531b700d90682b5b24245ab0e0551d3b436fa
Vini <vsgh-gouveia@hotmail.com>
2024-01-03 17:22:56 -0300

    Campos de produto atualizados

--- models/product.go
+++ models/product.go
@@ -9,14 +9,16 @@ import (
 type Product struct {
 	ID int32 `gorm:"primaryKey;"`
 
-	Name            string  `gorm:"type:varchar(255);notnull"`
-	Description     string  `gorm:"type:varchar(255);notnull"`
-	SKU             string  `gorm:"type:varchar(50);unique;notnull"`
-	Price           float32 `gorm:"notnull"`
-	Stock           int32   `gorm:"notnull;default:0"`
-	Active          bool    `gorm:"notnull;default:false"`
-	DiscountedPrice float32 `gorm:"notnull"`
-	Banner          string  `gorm:"type:varchar(255);notnull"`
+	Banner             string  `gorm:"type:varchar(255);notnull"`
+	Name               string  `gorm:"type:varchar(255);notnull"`
+	Description        string  `gorm:"type:varchar(255);notnull"`
+	Price              float32 `gorm:"notnull"`
+	DiscountPercentage float32 `gorm:"notnull;default:0"`
+	Stock              int32   `gorm:"notnull;default:0"`
+	Active             bool    `gorm:"notnull;default:false"`
+	Manufacturer       string  `gorm:"type:varchar(255);notnull;default:desconhecido"`
+	Print              string  `gorm:"type:varchar(255);notnull;default:desconhecido"`
+	SKU                string  `gorm:"type:varchar(50);unique;notnull"`
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
@@ -24,11 +26,9 @@ type Product struct {
 
 	TypeID     int32
 	CategoryID int32
-	SizeID     int32
 	ColorID    int32
 	Type       ProductType     `gorm:"foreignKey:TypeID;constraint:OnDelete:SET NULL;"`
 	Category   ProductCategory `gorm:"foreignKey:CategoryID;constraint:OnDelete:SET NULL;"`
-	Size       ProductSize     `gorm:"foreignKey:SizeID;constraint:OnDelete:SET NULL;"`
 	Color      ProductColor    `gorm:"foreignKey:ColorID;constraint:OnDelete:SET NULL;"`
 
 	Images []ProductImage `gorm:"foreignKey:ProductID"`
