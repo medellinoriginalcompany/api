c6ce2ddd25378719a0b6635bcc6522f046f88fa7
Vini <vsgh-gouveia@hotmail.com>
2024-01-03 17:37:36 -0300

    constraints atualizada para poder deletar produto

--- models/products_product_size.go
+++ models/products_product_size.go
@@ -4,10 +4,10 @@ type Products_product_size struct {
 	ID int32 `gorm:"primaryKey;"`
 
 	ProductID int32   `gorm:"notnull"`
-	Product   Product `gorm:"foreignKey:ProductID;constraint:OnDelete:SET NULL;"`
+	Product   Product `gorm:"foreignKey:ProductID;constraint:OnDelete:CASCADE;"`
 
 	SizeID int32       `gorm:"notnull"`
-	Size   ProductSize `gorm:"foreignKey:SizeID;constraint:OnDelete:SET NULL;"`
+	Size   ProductSize `gorm:"foreignKey:SizeID;constraint:OnDelete:CASCADE;"`
 }
 
 func (Products_product_size) TableName() string {
